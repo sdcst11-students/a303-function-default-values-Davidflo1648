1c34c35b9d6cad430053f103c3f2290cbeda5057
Davidflo1648 <144282155+Davidflo1648@users.noreply.github.com>
2023-11-01 13:09:45 -0700

    task2 done

--- task2.py
+++ task2.py
@@ -13,3 +13,18 @@ example assertion:
 assert multiplication(5) == [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60]
 assert multiplication(2,5) == [2, 4, 6, 8, 10]
 """
+def multiplication(num, end = 12):
+    #mult_of_nums = []
+    #while mult < end:
+        #mult = 1
+        #multiples = mult * num
+        #mult += 1
+        #mult_of_nums.append(multiples)
+
+    return [num * i for i in range(1, end + 1)]
+
+print(multiplication(2, 5))
+
+#if __name__ == "__main__":
+    #assert multiplication(5) == [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60]
+    #assert multiplication(2,5) == [2, 4, 6, 8, 10]
