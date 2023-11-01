95fd49fe96f5db8ad1f178102240245cd6b33f63
Davidflo1648 <144282155+Davidflo1648@users.noreply.github.com>
2023-11-01 13:08:31 -0700

    task1 done

--- task1.py
+++ task1.py
@@ -22,10 +22,13 @@ assert sentence("Hiya","Casey","Have you enjoyed your meal") == "Hiya Casey. Hav
 (2 points) 
 """
 
-def sentence():
-  return ""
+def sentence(greeting = "Hello", name = " Benjamin ", question = "How are you?"):
+  sentence = greeting + name + question
+  return sentence
+
+print(sentence("Hiya","Casey","Have you enjoyed your meal?"))
 
 
-if __name__ == "__main__":
-  assert sentence("Hello") == "Hello Benjamin. How are you"
-  assert sentence("Hiya","Casey","Have you enjoyed your meal") == "Hiya Casey. Have you enjoyed your meal"
+#if __name__ == "__main__":
+  #assert sentence("Hello") == "Hello Benjamin. How are you"
+  #assert sentence("Hiya","Casey","Have you enjoyed your meal") == "Hiya Casey. Have you enjoyed your meal"
